5341af95ddd67d6e01e001b1e514fab2d7875a01
Coca Razvan <razvancoca11@gmail.com>
2016-05-30 16:04:18 +0300

    c

--- src/gui/controllers/MainFrameController.java
+++ src/gui/controllers/MainFrameController.java
@@ -2,8 +2,12 @@ package gui.controllers;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import controller.FacturaController;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -16,10 +20,15 @@ import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.stage.WindowEvent;
+import model.Articol;
+import model.Factura;
+import model.InregistrareFactura;
+import model.SituatieStocuriTable;
 
 public class MainFrameController implements Initializable {
 
@@ -28,6 +37,10 @@ public class MainFrameController implements Initializable {
 
 	@FXML
 	private AnchorPane root2;
+	@FXML
+	private ChoiceBox<String> trimestru;
+
+	private static int TRIM_CURENT = Calendar.getInstance().get(Calendar.MONTH) / 4;
 
 	public void exitPressed() {
 		Platform.exit();
@@ -36,59 +49,127 @@ public class MainFrameController implements Initializable {
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
-		situatieByTrimestru(2);
+		initializareControale();
+		trimestru.getSelectionModel().select(TRIM_CURENT);
 
 	}
 
-	public void situatieByTrimestru(int trimestru) {
+	private void initializareControale() {
 
-		String luna1 = "Aprilie";
-		String luna2 = "Mai";
-		String luna3 = "Iunie";
+		trimestru.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
+			if (newSelection != null) {
+				TRIM_CURENT = trimestru.getSelectionModel().getSelectedIndex();
+				trimestru.getSelectionModel().select(TRIM_CURENT);
+				initializareGrafic(TRIM_CURENT + 1);
+			}
+		});
+
+		initializareGrafic(TRIM_CURENT);
+
+	}
+
+	public void initializareGrafic(int trimestru) {
+		String luna1;
+		String luna2;
+
+		double[] valuesLuna1 = getSituatieByLuna((trimestru - 1) * 4);
+		double[] valuesLuna2 = getSituatieByLuna((trimestru - 1) * 4 + 1);
+		double[] valuesLuna3 = getSituatieByLuna((trimestru - 1) * 4 + 2);
+
+		String luna3;
 		CategoryAxis xAxis = new CategoryAxis();
 		NumberAxis yAxis = new NumberAxis();
 		BarChart<String, Number> bc = new BarChart<>(xAxis, yAxis);
 		bc.setPrefWidth(400);
 		bc.setPrefHeight(300);
-		bc.setLayoutX(20);
-		bc.setLayoutY(60);
+		bc.setLayoutX(40);
+		bc.setLayoutY(10);
 		bc.setStyle("-fx-background-color:#FFF;-fx-margin:10px");
 		xAxis.setLabel("Luna");
 		yAxis.setLabel("Valoare");
 
 		if (trimestru == 1) {
 			bc.setTitle("Situatie trimestru I");
+			luna1 = "Ianuarie";
+			luna2 = "Februarie";
+			luna3 = "Martie";
 		} else if (trimestru == 2) {
 			bc.setTitle("Situatie trimestru II");
+			luna1 = "Aprilie";
+			luna2 = "Mai";
+			luna3 = "Iunie";
 
-		} else if (trimestru == 2) {
+		} else if (trimestru == 3) {
 			bc.setTitle("Situatie trimestru III");
+			luna1 = "Iulie";
+			luna2 = "August";
+			luna3 = "Septembrie";
 
 		} else {
 			bc.setTitle("Situatie trimestru IV");
+			luna1 = "Octombrie";
+			luna2 = "Noiembrie";
+			luna3 = "Decembrie";
 
 		}
 
 		XYChart.Series series1 = new XYChart.Series();
 		series1.setName("Aprovizionari");
-		series1.getData().add(new XYChart.Data(luna1, 25601.34));
-		series1.getData().add(new XYChart.Data(luna2, 20148.82));
-		series1.getData().add(new XYChart.Data(luna3, 10000));
+		series1.getData().add(new XYChart.Data(luna1, valuesLuna1[0]));
+		series1.getData().add(new XYChart.Data(luna2, valuesLuna2[0]));
+		series1.getData().add(new XYChart.Data(luna3, valuesLuna3[0]));
 
 		XYChart.Series series2 = new XYChart.Series();
 		series2.setName("Vanzari");
-		series2.getData().add(new XYChart.Data(luna1, 57401.85));
-		series2.getData().add(new XYChart.Data(luna2, 41941.19));
-		series2.getData().add(new XYChart.Data(luna3, 45263.37));
+		series2.getData().add(new XYChart.Data(luna1, valuesLuna1[1]));
+		series2.getData().add(new XYChart.Data(luna2, valuesLuna2[1]));
+		series2.getData().add(new XYChart.Data(luna3, valuesLuna3[1]));
 
 		XYChart.Series series3 = new XYChart.Series();
 		series3.setName("Sold final luna");
-		series3.getData().add(new XYChart.Data(luna1, 25601.34));
-		series3.getData().add(new XYChart.Data(luna2, 20148.82));
-		series3.getData().add(new XYChart.Data(luna3, 10000));
+		series3.getData().add(new XYChart.Data(luna1, valuesLuna1[2]));
+		series3.getData().add(new XYChart.Data(luna2, valuesLuna2[2]));
+		series3.getData().add(new XYChart.Data(luna3, valuesLuna3[2]));
 
 		root2.getChildren().add(bc);
 		bc.getData().addAll(series1, series2, series3);
+	}
+
+	public double[] getSituatieByLuna(int luna) {
+
+		double[] values = { 2221, 2, 3, 4 };
+
+		List<InregistrareFactura> inregistrari = new ArrayList<>();
+		List<Factura> facturi = new FacturaController().selectAll();
+		List<Articol> articole = new ArrayList<>();
+
+		double stoc1,stoc2;
+
+		// preluare facturi din luna curenta
+		for (Factura f : facturi) {
+			if (f.getDataDocument().getMonth() == luna) {
+				inregistrari.addAll(f.getArticole());
+			}
+		}
+
+		// preluare articole utilizate in luna curenta
+		for (InregistrareFactura iff : inregistrari) {
+			articole.add(iff.getArticol());
+		}
+
+		for (InregistrareFactura iff : inregistrari) {
+			// daca e intrare
+			if (iff.getTip() == 0) {
+				values[0]+=iff.getValoare();
+			} else {
+				values[1]+=iff.getValoare();
+			}
+
+			values[2]= values[1]-values[0];
+		}
+
+		return values;
+
 	}
 
 	public void clientiPressed(ActionEvent event) {
